b5604c77859a20ad62e5b5468078fd443f7ba28f
ESAKKI-029 <108513721+ESAKKI-029@users.noreply.github.com>
2023-08-22 16:09:36 +0530

    Delete PutAndDeleteMapping directory

--- PutAndDeleteMapping/src/main/java/com/example/demo/repository/HotelRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.HotelModel;
-
-public interface HotelRepo extends JpaRepository<HotelModel, Integer> {
-
-}
